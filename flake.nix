366e0844a1aa0cecb36447d51fe075e1d93fa508
Rasmus Kivi <rallejansson37@gmail.com>
2026-03-13 10:39:51 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "flux-local nixpkg flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          flux-local = pkgs.callPackage ./default.nix { };
+          default = self.packages.${system}.flux-local;
+        };
+      }
+    );
+}
